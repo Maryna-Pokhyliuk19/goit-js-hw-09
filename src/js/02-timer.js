e5213d13ebe462341af6071429a8b07ebe8004a6
Maryna Pokhyliuk <marinapohiluk19@gmail.com>
2022-08-26 13:18:16 +0300

    task 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,111 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import Notiflix from 'notiflix';
+
+flatpickr("#datetime-picker", {
+ enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        timer.targetTime = selectedDates[0].getTime()
+        if (timer.targetTime < Date.now()) {
+            Notiflix.Notify.failure(`wrong date`)
+return
+        }
+        refs.buttonStart.disabled = false
+        
+  },
+});
+
+
+const refs = {
+    buttonStart: document.querySelector('button[data-start]'),
+    input: document.querySelector('#datetime-picker'),
+    days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+
+
+class Timer  {
+    constructor({ onTick }) {
+        this.intervalID = null;
+        this.isActive = false;
+        this.onTick = onTick;
+        this.targetTime = null;
+
+    }
+    
+    start() {
+        
+        this.isActive = true;
+        this.checkIsActive()
+
+        this.intervalID = setInterval(() => {
+            const currentTime = Date.now()
+
+            const deltaTime = this.targetTime - currentTime;
+            
+            if (deltaTime < 1000) {
+                
+                clearInterval(this.intervalID)
+                this.isActive = false
+                this.checkIsActive()
+                
+            }
+            const time = this.convertMs(deltaTime)
+            this.checkIsActive()
+            this.onTick(time);
+        }, 1000)
+
+    }
+    convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = this.addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+    }
+    addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+    }
+    checkIsActive() {
+        refs.buttonStart.disabled = this.isActive
+        refs.input.disabled = this.isActive
+    }
+}
+
+
+const timer = new Timer({
+    onTick: updateClockface,
+})
+
+refs.buttonStart.addEventListener('click', () => {
+    timer.start()
+})
+
+function updateClockface({ days, hours, minutes, seconds }) {
+
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.minutes.textContent = minutes;
+    refs.seconds.textContent = seconds;
+}
+
+
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const form = document.querySelector("form")
+
+form.addEventListener('submit', onFormsubmit)
+
+
+function onFormsubmit(evt) {
+  evt.preventDefault();
+  const delay = Number(evt.currentTarget.delay.value)
+  const step = Number(evt.currentTarget.step.value)
+  const amount = Number(evt.currentTarget.amount.value)
+
+  listOfPromises(delay, step, amount)
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+
+  })
+  } 
+
+function listOfPromises(delay, step, amount) {
+  let stepDelay = delay
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, stepDelay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    stepDelay += step
   }
 }
+
